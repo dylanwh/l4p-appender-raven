d3ea5675b36cedb78dcb6a2b1f9191ab187e9111
Jerome Eteve <jerome@broadbean.com>
2014-07-09 11:02:30 +0100

    Basic building

---
+++ cpanfile
@@ -0,0 +1,2 @@
+requires 'Moose' , '>= 2.0801';
+requires 'Sentry::Raven', '>= 0.05';
--- lib/Log/Log4perl/Appender/Raven.pm
+++ lib/Log/Log4perl/Appender/Raven.pm
@@ -1,5 +1,22 @@
-use strict;
-use warnings;
 package Log::Log4perl::Appender::Raven;
 
+use Moose;
+
+use Sentry::Raven;
+
+has 'sentry_dsn' => ( is => 'ro', isa => 'Str' , required => 1);
+has 'sentry_timeout' => ( is => 'ro' , isa => 'Int' ,required => 1 , default => 5 );
+
+has 'raven' => ( is => 'ro', isa => 'Sentry::Raven', lazy_build => 1);
+has 'context' => ( is => 'ro' , isa => 'HashRef', default => sub{ {}; });
+
+sub _build_raven{
+    my ($self) = @_;
+    return Sentry::Raven->new( sentry_dsn => $self->sentry_dsn,
+                               timeout => $self->sentry_timeout,
+                               %{$self->context()}
+                             );
+}
+
+__PACKAGE__->meta->make_immutable();
 1;
